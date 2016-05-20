2c61e11f3f812709ecdfe58a9d6172d9f2befcec
Emily Pepke <elpepke41@gmail.com>
2016-05-20 15:18:25 -0700

    set up authentication

--- web/router.ex
+++ web/router.ex
@@ -2,12 +2,26 @@ defmodule Peepchat.Router do
   use Peepchat.Web, :router
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug :accepts, ["json", "json-api"]
+  end
+
+  # Authenticated Requests
+  pipeline :api_auth do
+    plug :accepts, ["json", "json-api"]
+    plug Guardian.Plug.VerifyHeader
+    plug Guardian.Plug.LoadResource
   end
 
   scope "/api", Peepchat do
     pipe_through :api
-    # Route stuff to the SessionController
-    resources "session", SessionController, only: [:index]
+    # Registration
+    post "register", RegistrationController, :create
+    # Login
+    post "token", SessionController, :create, as: :login
+  end
+
+  scope "/api", Peepchat do
+    pipe_through :api_auth
+    get "/user/current", UserController, :current
   end
 end
